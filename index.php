4617d27748846563920f1e4a1e29fca16c24d4cc
Xzevs <rasmus.karlsson@elev.ga.ntig.se>
2019-11-07 09:34:18 +0100

    Funkar bättre med ny kod!

--- index.php
+++ index.php
@@ -7,17 +7,23 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <div class="form-group">
-            <label for="username" class="form-label">Username:</label>
+    <main class="container mt-5">
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label for="username" class="form-label">Username:</label>
     <input type="text" name="username" class="form-control">
 </div>
     <input type="submit" value="Skicka" class="btn btn-primary">
     </form>
 
     <div class="mt-5">
+    <h1>
     <?php
-    echo "<pre class=\"lead\">" . print_r($_POST, 1) . "</pre";
+    //echo "<pre class=\"lead\">" . print_r($_POST, 1) . "</pre";
+    $myname = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    echo $myname;
     ?>
+    </h1>
+
 </body>
 </html>
