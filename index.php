84d7f0017d3b33595c5840c8e62bdf53289d17f4
Xzevs <rasmus.karlsson@elev.ga.ntig.se>
2019-11-07 09:17:28 +0100

    Gjorde en ruta där man kan skriva in sitt username.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <div class="form-group">
+            <label for="username" class="form-label">Username:</label>
+    <input type="text" name="username" class="form-control">
+</div>
+    <input type="submit" value="Skicka" class="btn btn-primary">
+    </form>
+
+    <div class="mt-5">
+    <?php
+    echo "<pre class=\"lead\">" . print_r($_POST, 1) . "</pre";
+    ?>
+</body>
+</html>
